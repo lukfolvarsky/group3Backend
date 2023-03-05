29665de01bb8addf17316c3b806b175a137e9745
Robert Lieblang <roberttheanimal01@gmail.com>
2023-03-05 16:31:13 -0600

    Add nodemon and "check if user exists"

--- backend/groupThreeRoutes/usersInformation.js
+++ backend/groupThreeRoutes/usersInformation.js
@@ -20,16 +20,27 @@ router.get('/test', (req, res) => {
 
 
 
-
-
-
 //Example of running query:
-//  http://localhost:3301/usersInformation/users/chronotype/results?uid=1234abcd
-// YOu can change the UID by replacing '1234acbd'
+//  http://localhost:3301/usersInformation/users/chronotype/results?uid=1234abcd 
+// YOu can change the UID by replacing '1234acbd' 
 router.get('/users/chronotype/results', (req, res) => {
     const uid = req.query.uid;
     client.query(
       `SELECT * FROM chronotypes WHERE id IN (SELECT chronotype_id FROM users WHERE uid = $1)`,
+      [uid],
+      (err, result) => {
+        if (!err) {
+          res.send(result.rows);
+        }
+      }
+    );
+  });
+
+  // Check if user exists in the Database
+  router.get('/users/exists', (req, res) => {
+    const uid = req.query.uid;
+    client.query(
+      `SELECT * FROM users WHERE uid = $1`,
       [uid],
       (err, result) => {
         if (!err) {
